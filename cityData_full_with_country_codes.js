d3c1d7a92b6a4b3976daf3e4a59e39fef45fb20c
andreaskouv96 <74777992+andreaskouv96@users.noreply.github.com>
2024-11-02 16:33:16 +0200

    Update cityData_full_with_country_codes.js

--- cityData_full_with_country_codes.js
+++ cityData_full_with_country_codes.js
@@ -1,7 +1,82 @@
 const cityData = [
-{'city': 'Zurich', 'country': 'Switzerland', 'country_code': 'CH', 'created_at': "'2024-10-14 09:18:55.607512+00'", 'mobility_solutions': {'Car-sharing Apps have reduced congestion': 43.6, 'Apps that direct you to an available parking space have reduced': 44.5, 'Bicycle hiring has reduced congestion': 55.6, 'Online scheduling and ticket sales has made public transport easy': 79.7, 'The city provides information on traffic congestion through mobile apps': 61.8}, 'traffic_congestion_not_a_problem': 41.0, 'public_transport_satisfactory': 80.0, 'smart_city_ranking': "'AAA'"},
-{'city': 'Singapore', 'country': 'Singapore', 'country_code': 'SG', 'created_at': 'null', 'mobility_solutions': {'Car-sharing Apps have reduced congestion': 57.6, 'Apps that direct you to an available parking space have reduced': 57.5, 'Bicycle hiring has reduced congestion': 56.7, 'Online scheduling and ticket sales has made public transport easy': 63.2, 'The city provides information on traffic congestion through mobile apps': 75.7}, 'traffic_congestion_not_a_problem': 50.0, 'public_transport_satisfactory': 78.5, 'smart_city_ranking': "'A'"},
-{'city': 'Mexico City', 'country': 'Mexico', 'country_code': 'MX', 'created_at': 'null', 'mobility_solutions': {'Car-sharing Apps have reduced congestion': 42.6, 'Apps that direct you to an available parking space have reduced': 43.0, 'Bicycle hiring has reduced congestion': 52.8, 'Online scheduling and ticket sales has made public transport easy': 55.1, 'The city provides information on traffic congestion through mobile apps': 63.4}, 'traffic_congestion_not_a_problem': 23.4, 'public_transport_satisfactory': 33.0, 'smart_city_ranking': "'C'"},
-{'city': 'Rome', 'country': 'Italy', 'country_code': 'IT', 'created_at': 'null', 'mobility_solutions': {'Car-sharing Apps have reduced congestion': 32.4, 'Apps that direct you to an available parking space have reduced': 35.0, 'Bicycle hiring has reduced congestion': 35.1, 'Online scheduling and ticket sales has made public transport easy': 57.1, 'The city provides information on traffic congestion through mobile apps': 44.5}, 'traffic_congestion_not_a_problem': 11.8, 'public_transport_satisfactory': 17.7, 'smart_city_ranking': "'CCC'"},
-{'city': 'Bogota', 'country': 'Colombia', 'country_code': 'CO', 'created_at': 'null', 'mobility_solutions': {'Car-sharing Apps have reduced congestion': 36.4, 'Apps that direct you to an available parking space have reduced': 39.4, 'Bicycle hiring has reduced congestion': 46.5, 'Online scheduling and ticket sales has made public transport easy': 48.8, 'The city provides information on traffic congestion through mobile apps': 58.7}, 'traffic_congestion_not_a_problem': 16.1, 'public_transport_satisfactory': 22.9, 'smart_city_ranking': "'D'"},
+    {
+        'city': 'Zurich', 
+        'country': 'Switzerland', 
+        'country_code': 'CH', 
+        'created_at': '2024-10-14 09:18:55.607512+00', 
+        'mobility_solutions': {
+            'Car-sharing Apps have reduced congestion': 43.6,
+            'Apps that direct you to an available parking space have reduced': 44.5,
+            'Bicycle hiring has reduced congestion': 55.6,
+            'Online scheduling and ticket sales has made public transport easy': 79.7,
+            'The city provides information on traffic congestion through mobile apps': 61.8
+        }, 
+        'traffic_congestion_not_a_problem': 41.0, 
+        'public_transport_satisfactory': 80.0, 
+        'smart_city_ranking': 'AAA'
+    },
+    {
+        'city': 'Singapore', 
+        'country': 'Singapore', 
+        'country_code': 'SG', 
+        'created_at': null, 
+        'mobility_solutions': {
+            'Car-sharing Apps have reduced congestion': 57.6,
+            'Apps that direct you to an available parking space have reduced': 57.5,
+            'Bicycle hiring has reduced congestion': 56.7,
+            'Online scheduling and ticket sales has made public transport easy': 63.2,
+            'The city provides information on traffic congestion through mobile apps': 75.7
+        }, 
+        'traffic_congestion_not_a_problem': 50.0, 
+        'public_transport_satisfactory': 78.5, 
+        'smart_city_ranking': 'A'
+    },
+    {
+        'city': 'Mexico City', 
+        'country': 'Mexico', 
+        'country_code': 'MX', 
+        'created_at': null, 
+        'mobility_solutions': {
+            'Car-sharing Apps have reduced congestion': 42.6,
+            'Apps that direct you to an available parking space have reduced': 43.0,
+            'Bicycle hiring has reduced congestion': 52.8,
+            'Online scheduling and ticket sales has made public transport easy': 55.1,
+            'The city provides information on traffic congestion through mobile apps': 63.4
+        }, 
+        'traffic_congestion_not_a_problem': 23.4, 
+        'public_transport_satisfactory': 33.0, 
+        'smart_city_ranking': 'C'
+    },
+    {
+        'city': 'Rome', 
+        'country': 'Italy', 
+        'country_code': 'IT', 
+        'created_at': null, 
+        'mobility_solutions': {
+            'Car-sharing Apps have reduced congestion': 32.4,
+            'Apps that direct you to an available parking space have reduced': 35.0,
+            'Bicycle hiring has reduced congestion': 35.1,
+            'Online scheduling and ticket sales has made public transport easy': 57.1,
+            'The city provides information on traffic congestion through mobile apps': 44.5
+        }, 
+        'traffic_congestion_not_a_problem': 11.8, 
+        'public_transport_satisfactory': 17.7, 
+        'smart_city_ranking': 'CCC'
+    },
+    {
+        'city': 'Bogota', 
+        'country': 'Colombia', 
+        'country_code': 'CO', 
+        'created_at': null, 
+        'mobility_solutions': {
+            'Car-sharing Apps have reduced congestion': 36.4,
+            'Apps that direct you to an available parking space have reduced': 39.4,
+            'Bicycle hiring has reduced congestion': 46.5,
+            'Online scheduling and ticket sales has made public transport easy': 48.8,
+            'The city provides information on traffic congestion through mobile apps': 58.7
+        }, 
+        'traffic_congestion_not_a_problem': 16.1, 
+        'public_transport_satisfactory': 22.9, 
+        'smart_city_ranking': 'D'
+    }
 ];
