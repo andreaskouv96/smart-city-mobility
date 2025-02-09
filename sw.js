9f9bf311dd6abfe7757d19344b3b7e6492fdc6fb
andreaskouv96 <74777992+andreaskouv96@users.noreply.github.com>
2025-02-09 18:52:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,26 @@
 
+//Service Worker
+const CACHE_NAME = "smart-city-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/cityData_full_with_country_codes.js",
+  "/smartSuggestions.js",
+  "/logo.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
